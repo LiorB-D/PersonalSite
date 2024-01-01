2b7e1bc23a408b887c8aff00d411e2d09bc57e71
Lior Ben-David <liorbd@Outlook.com>
2024-01-01 16:40:53 -0500

    More meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,19 +22,25 @@ export default function RootLayout({
           property="og:image"
           content="https://cdn.aarp.net/content/dam/aarp/travel/trips/2020/08/1140-sunset-at-shenandoah-national-park.jpg"
         />
-
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:image"
+          property="twitter:image"
+          content="https://cdn.aarp.net/content/dam/aarp/travel/trips/2020/08/1140-sunset-at-shenandoah-national-park.jpg"
+        />
+        <meta name="twitter:card" property="twitter:card" content="summary_large_image" />
         <meta
           property="twitter:domain"
           content={`liorbd.me`}
         />
-        <meta property="twitter:url" content="" />
+        <meta property="twitter:url" name="twitter:url" content="" />
         <meta
           name="twitter:title"
+          property="twitter:title"
           content={"Lior Ben-David"}
         />
         <meta
           name="twitter:description"
+          property="twitter:description"
           content={"Lior Ben-David Personal Website"}
         />
         <meta name="twitter:image" content="https://cdn.aarp.net/content/dam/aarp/travel/trips/2020/08/1140-sunset-at-shenandoah-national-park.jpg"/>
