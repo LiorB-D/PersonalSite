f52495e14bee638cc9d6b1911809a8c1a2ed954c
Lior Ben-David <liorbd@outlook.com>
2024-01-12 18:51:30 -0500

    Setup cms util

---
+++ src/app/util/cms.ts
@@ -0,0 +1,56 @@
+export type Article = {
+	title: string;
+	notionId: string;
+};
+
+export const SPOTIFYEMBEDLINKS = [
+	'https://open.spotify.com/embed/track/1kq9lzEHoY3p8naHWpqNlh?utm_source=generator',
+	'https://open.spotify.com/embed/episode/1etIR5eEvpFKsWihW2cax1?utm_source=generator',
+	'https://open.spotify.com/embed/track/2S7vZHfK6QTtK30pzAqBLP?utm_source=generator',
+];
+
+export const MATH_ARTICLES: Article[] = [
+	{
+		title: 'The Cap Set Problem',
+		notionId: '05d2cd5883ab445aab4ab84c8700cc7b',
+	},
+	{
+		title: 'A Survey of Mathematical Spaces',
+		notionId: '8dcf23fd3b044fd9ab36d92aada5f88d',
+	},
+	{
+		title: 'Simplicial Complexes: Giving Shape to Data',
+		notionId: 'd4f0c02cb1f0431aa0ef381c739b830d',
+	},
+];
+
+export const DEVLOGS: Article[] = [
+	{
+		title: 'Teaching Cars to Drive with Neuroevolution',
+		notionId: '22e0d206df6046ddbfa517aab17ed970',
+	},
+	{
+		title: 'Building VR Websites in Javascript',
+		notionId: 'b587cacaa6684c7daaecf04bddde794b',
+	},
+];
+
+export const GENBLOGS: Article[] = [
+	{
+		title: 'The First Ad I Ever Served',
+		notionId: 'd89b4efb5dbc438a9d5a05c4968b5518',
+	},
+	{
+		title: 'In Defense of Hammers In Search of Nails',
+		notionId: '4abf2949130a4225a5f921d5f318ab3d',
+	},
+];
+
+export function getTitleFromId(id: string): string {
+	const allBlogs = MATH_ARTICLES.concat(DEVLOGS).concat(GENBLOGS);
+	const matchingTitle = allBlogs.filter((article) => article.notionId === id);
+	if (matchingTitle) {
+		return matchingTitle[0].title;
+	}
+	return 'Error';
+}
