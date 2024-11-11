1ec01523d4590a434c878e5152bd290afd2c0a0d
Lior Ben-David <liorbd@outlook.com>
2024-11-11 10:48:44 -0500

    playground tester

---
+++ src/app/tools/playgroundtester/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+
+  return <div>
+    <h1>Playground tester</h1>
+    <iframe src="https://learn.enki.com/public/python-playground?readonly=true&startingcode=for%20i%20in%20range%281%2C%20101%29%3A%0A%20%20%20%20print%28i%29" height="600px" width="550px" className="codeplayground"></iframe>
+  </div>
+}
