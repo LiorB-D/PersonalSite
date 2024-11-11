c82148b5d9535b0670cd2575917f874757557432
Lior Ben-David <liorbd@outlook.com>
2024-11-11 11:49:47 -0500

    more

--- src/app/tools/playgroundtester/page.tsx
+++ src/app/tools/playgroundtester/page.tsx
@@ -5,5 +5,18 @@ export default async function Page() {
     <iframe src="https://staging.learn.enki.com/public/python-playground?readonly=true&startingcode=for%20i%20in%20range%281%2C%20101%29%3A%0A%20%20%20%20print%28i%29" height="600px" width="550px" className="codeplayground"></iframe>
     <p>Intermediate text</p>
     <iframe src="https://staging.learn.enki.com/public/python-playground?readonly=true&startingcode=for%20i%20in%20range%281%2C%20101%29%3A%0A%20%20%20%20print%28i%29" height="600px" width="550px" className="codeplayground"></iframe>
+    <h1>Playground tester</h1>
+    <iframe src="https://staging.learn.enki.com/public/python-playground?readonly=true&startingcode=for%20i%20in%20range%281%2C%20101%29%3A%0A%20%20%20%20print%28i%29" height="600px" width="550px" className="codeplayground"></iframe>
+    <p>Intermediate text</p>
+    <iframe src="https://staging.learn.enki.com/public/python-playground?readonly=true&startingcode=for%20i%20in%20range%281%2C%20101%29%3A%0A%20%20%20%20print%28i%29" height="600px" width="550px" className="codeplayground"></iframe>
+    <h1>Playground tester</h1>
+    <iframe src="https://staging.learn.enki.com/public/python-playground?readonly=true&startingcode=for%20i%20in%20range%281%2C%20101%29%3A%0A%20%20%20%20print%28i%29" height="600px" width="550px" className="codeplayground"></iframe>
+    <p>Intermediate text</p>
+    <iframe src="https://staging.learn.enki.com/public/python-playground?readonly=true&startingcode=for%20i%20in%20range%281%2C%20101%29%3A%0A%20%20%20%20print%28i%29" height="600px" width="550px" className="codeplayground"></iframe>
+    <h1>Playground tester</h1>
+    <iframe src="https://staging.learn.enki.com/public/python-playground?readonly=true&startingcode=for%20i%20in%20range%281%2C%20101%29%3A%0A%20%20%20%20print%28i%29" height="600px" width="550px" className="codeplayground"></iframe>
+    <p>Intermediate text</p>
+    <iframe src="https://staging.learn.enki.com/public/python-playground?readonly=true&startingcode=for%20i%20in%20range%281%2C%20101%29%3A%0A%20%20%20%20print%28i%29" height="600px" width="550px" className="codeplayground"></iframe>
+
   </div>
 }
